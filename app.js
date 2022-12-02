4339ecb98661280d5d0aa797bd458acea8a26cb5
SeJoMaCode <seth.manche@sckans.edu>
2022-12-02 15:04:38 -0600

    added CORS

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require("./app_api/models/db.js")
+let cors = require('cors');
 
 var indexRouter = require('./app_server/routes/index');
 // var usersRouter = require('./app_server/routes/users');
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
