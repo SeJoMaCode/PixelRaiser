eda627f574635aa19b8256d96207324123e42e5f
SeJoMaCode <seth.manche@sckans.edu>
2022-12-02 20:28:57 -0600

    added cors params

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(cors({origin: true, credentials: true}));
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
@@ -41,6 +41,11 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error', { title: 'ERROR'});
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 module.exports = app;
